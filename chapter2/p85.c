cdffd42f31b2b1f55c6d68c7b6b8259821e066eb
Qiyuan Pu <pqy7172@gmail.com>
2016-10-26 21:34:35 +0800

    Add some notes for this program

--- chapter2/p85.c
+++ chapter2/p85.c
@@ -1,3 +1,11 @@
+/*
+  One question of this program is that:
+  now that we set the posix_version = 0, why we judge "posix_version == 0" at line 29?
+
+  As original program no main function, I add the main function.
+ */
+
+
 #include "apue.h"
 #include <errno.h>
 #include <limits.h>
