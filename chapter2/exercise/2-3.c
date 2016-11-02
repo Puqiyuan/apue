2ee4363d248cd5af26c479341c44658b106b7807
Qiyuan Pu <pqy7172@gmail.com>
2016-11-02 22:18:25 +0800

    Solution of 2.3

---
+++ chapter2/exercise/2-3.c
@@ -0,0 +1,57 @@
+/*
+  page number in paper book:52    
+  I add main function for test.
+
+  Your output should be like this after you running:
+  pqy@sda1:~/.../chapter2$ ./a.out 
+  The max file can be open: 65536
+  
+ */
+
+
+#include "apue.h"
+#include <errno.h>
+#include <limits.h>
+
+#ifdef OPEN_MAX
+static long openmax = OPNE_MAX;
+#else
+static long openmax = 0;
+#endif
+
+#define OPEN_MAX_GUESS 256
+
+long open_max(void)
+{
+  if (openmax == 0)
+    {
+      errno = 0;
+      
+      // what codes added starts:
+      if (sysconf(_SC_OPEN_MAX == (long int)LONG_MAX))
+	exit(0);
+      // added code end
+
+	
+      if ()
+      if ((openmax = sysconf(_SC_OPEN_MAX)) < 0)
+	{
+	  if (errno == 0)
+	    openmax = OPEN_MAX_GUESS;
+
+	  else
+	    err_sys("sysconf error for _SC_OPEN_MAX");
+	}
+    }
+
+  return (openmax);
+}
+
+int main(int argc, char *argv[])
+{
+  openmax = open_max();
+  printf("The max file can be open: %ld\n", openmax);
+  
+  return 0;
+}
+
