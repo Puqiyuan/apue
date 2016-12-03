6c249b07c365c40cd531cd0e716ab3e365da2ecf
Qiyuan Pu <pqy7172@gmail.com>
2016-12-03 13:52:30 +0800

    add outputs and running

--- chapter3/example_programs/p102.c
+++ chapter3/example_programs/p102.c
@@ -1,4 +1,19 @@
 //page number in paper book: 68
+/*
+  the output should be like this:
+  pqy@sda1:~/.../bin$ ./p102
+  pqy@sda1:~/.../bin$ ls -sl file.hole 
+  8 -rw-r--r-- 1 pqy pqy 16394 Dec  3 13:42 file.hole
+  pqy@sda1:~/.../bin$ od -c file.hole 
+  0000000   a   b   c   d   e   f   g   h   i   j  \0  \0  \0  \0  \0  \0
+  0000020  \0  \0  \0  \0  \0  \0  \0  \0  \0  \0  \0  \0  \0  \0  \0  \0
+  *
+  0040000   A   B   C   D   E   F   G   H   I   J
+  0040012
+
+  The explanation in the book on the page 68.
+ */
+
 
 
 #include "apue.h"
