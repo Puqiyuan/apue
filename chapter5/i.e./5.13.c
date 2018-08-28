07f4d3896a5b493e32ce3bafc75d405533801acf
Qiyuan Pu <pqy7172@gmail.com>
2018-08-28 22:11:56 +0800

    complete of 5.13

---
+++ chapter5/i.e./5.13.c
@@ -0,0 +1,49 @@
+/*
+  ~/.../i.e.$ ./a.out 
+  trying to create first temp file...
+  temp name = /tmp/dirmjPIZA
+  file exists
+  tring to create second temp file...
+  Segmentation fault
+*/
+
+#include <apue.h>
+#include <errno.h>
+
+void make_temp(char *template);
+
+int main(int argc, char *argv[])
+{
+	char good_template[] = "/tmp/dirXXXXXX"; // right way
+	char *bad_template = "/tmp/dirXXXXXX";
+
+	printf("trying to create first temp file...\n");
+	make_temp(good_template);
+	printf("tring to create second temp file...\n");
+	make_temp(bad_template);
+	
+	exit(0);
+}
+
+void make_temp(char *template)
+{
+	int fd;
+	struct stat sbuf;
+
+	if ((fd = mkstemp(template)) < 0)
+		err_sys("can't create temp file");
+	printf("temp name = %s\n", template);
+	close(fd);
+	if (stat(template, &sbuf) < 0)
+		{
+			if (errno == ENOENT)
+				printf("file doesn't exist\n");
+			else
+				err_sys("stat failed");
+		}
+	else
+		{
+			printf("file exists\n");
+			unlink(template);
+		}
+}
